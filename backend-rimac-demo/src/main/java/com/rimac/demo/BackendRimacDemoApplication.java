57dd40e2076c15f835230611a73d27639213f54a
Julio Wong <julioo.wong@gmail.com>
2022-03-05 21:27:23 -0500

    project backend rimac

---
+++ backend-rimac-demo/src/main/java/com/rimac/demo/BackendRimacDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rimac.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRimacDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRimacDemoApplication.class, args);
+	}
+
+}
---
+++ backend-rimac-demo/src/test/java/com/rimac/demo/BackendRimacDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimac.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRimacDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
